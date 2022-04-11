9fd8d6a9fead690f85d042922005236df1462136
Gustavo Valentin <gstvalentin@hotmail.com>
2022-04-11 17:47:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,22 @@ def computer_guess(x):
     guess = random.randint(low, high)
     while user_guess != guess:
         print(f'Seu número é {guess}')
-        sleep(1)
         if guess >= high / 2 and guess <= high and user_guess <= high / 2 :
             print('Erooou, seu número ainda está muito alto\n')
             guess = random.randint(1, high / 2)
+            sleep(1)
         elif guess >= high / 2 and guess <= high and user_guess >= high / 2 :
             print('Ta perto, continua que você chega lá pczin top\n')
             guess = random.randint(high / 2, high)
+            sleep(1)
         elif guess <= high / 2 and guess >= low and user_guess >= high / 2:
             print('Erooou, seu número ainda está muito baixo\n')
             guess = random.randint(high / 2, high)
+            sleep(1)
         elif guess <= high / 2 and guess >= low and user_guess <= high / 2:
             print('Ta perto, continua que você chega lá pczin top\n')
             guess = random.randint(1, high / 2)
+            sleep(1)
     print(f'Aee Caraio, acertou!! \n \
             O numéro escolhido foi o {user_guess} ')
 
